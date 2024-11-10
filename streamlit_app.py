896f88b8d6a461079262972955525c9b0a4dbe40
Mohamed Wassim Hamra <wassimhamraa@gmail.com>
2024-11-10 15:12:50 +0100

    first push

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,8 +70,8 @@ st.markdown(
 # Display text inside the box
 
 if prediction_proba > 0.5:
-    st.markdown('<div class="box"><h3 style="color:#0d0f0c;">Client is likely to churn 😢</h3></div>', unsafe_allow_html=True)
+    st.markdown('<div class="box"><h2 style="color:#0d0f0c;">Client is likely to churn 😢</h2></div>', unsafe_allow_html=True)
 else:
-    st.markdown('<div class="box"><h3 style="color:#0d0f0c;">Client is not likely to churn 😌</h3></div>', unsafe_allow_html=True)
+    st.markdown('<div class="box"><h2 style="color:#0d0f0c;">Client is not likely to churn 😌</h2></div>', unsafe_allow_html=True)
 
 st.write(f"Churn Probability: {round(prediction_proba * 100,1)}%")

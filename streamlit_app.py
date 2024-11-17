9fca68b2e36ce50e23c0d8f5f318b10ca9d87c8c
Mohamed Wassim Hamra <wassimhamraa@gmail.com>
2024-11-17 22:04:56 +0100

    first push

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ reverse_word_index = {value: key for key, value in word_index.items()}
 # Load the pre-trained model with ReLU activation
 model = load_model('rnn.h5')
 
+
 # Function to preprocess user input
 def preprocess_text(text):
     words = text.lower().split()
@@ -21,24 +22,29 @@ def preprocess_text(text):
 
 
 import streamlit as st
+
 ## streamlit app
 st.title('IMDB Movie Review Sentiment Analysis')
 st.write('Enter a movie review to classify it as positive or negative.')
 
 # User input
 user_input = st.text_area('Movie Review')
-
 if st.button('Classify'):
 
-    preprocessed_input=preprocess_text(user_input)
+    preprocessed_input = preprocess_text(user_input)
 
     ## MAke prediction
-    prediction=model.predict(preprocessed_input)
-    sentiment='Positive ✅' if prediction[0][0] > 0.5 else 'Negative ❌'
+    prediction = model.predict(preprocessed_input)
+    sentiment = 'Positive ✅' if prediction[0][0] > 0.5 else 'Negative ❌'
 
     # Display the result
     st.write(f'Sentiment: {sentiment}')
-    st.write(f'Prediction Score: {prediction[0][0]}')
+    if sentiment == 'Negative ❌':
+        st.write(f'Prediction Score: {round(100 - prediction[0][0] * 100, 1)}%')
+        st.write('⚠️ Note: This model has been developed for learning purposes and may not always provide accurate results. It is primarily trained on lengthy reviews, which might affect its performance when analyzing shorter ones.')
+
+    else:
+        st.write(f'Prediction Score: {round(prediction[0][0] * 100, 1)}%')
+        st.write('⚠️ Note: This model is made for learning and may not always provide accurate results. It is primarily trained on long reviews, which might affect its performance when analyzing shorter ones.')
 else:
     st.write('Please enter a movie review.')
-
